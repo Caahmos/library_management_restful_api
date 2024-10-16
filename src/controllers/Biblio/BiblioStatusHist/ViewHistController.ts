7f2e5baf7007f5eb2947f91ff2524b00d5ffb1e2
Caahmos <dev.alexmorales@gmail.com>
2024-10-16 15:32:12 -0300

    docs: Added view hist/holds docs

--- src/controllers/Biblio/BiblioStatusHist/ViewHistController.ts
+++ src/controllers/Biblio/BiblioStatusHist/ViewHistController.ts
@@ -5,13 +5,13 @@ class ViewHistController{
     static async handle(req: Request, res: Response){
         const userId = req.userid;
         const userroles = req.userroles;
-        const copyid = Number(req.params.copyid);
+        const bibid = Number(req.params.bibid);
         
         if(!userId) return res.status(422).json({ type: 'error', message: 'Usuário não autenticado!'});
         if(!userroles.admin_flg) return res.status(422).json({ type: 'error', message: 'Usuário não tem permissão!'});
 
         try{
-            const foundHist = await ViewHistService.execute(copyid);
+            const foundHist = await ViewHistService.execute(bibid);
             res.status(200).json({ type: 'success', message: 'Histórico encontrado com sucesso!', foundHist});
         }catch(err: any){
             res.status(422).json({ type: 'error', message: err.message});
