1c4b6fe2833d13258d8818e4cad437e406542b49
Cauã Morales <cauam@etehort.ps.sp>
2025-07-07 14:32:57 -0300

    feat: Prisma Schema Modified

--- src/services/Biblio/BiblioStatusHist/ViewHistsService.ts
+++ src/services/Biblio/BiblioStatusHist/ViewHistsService.ts
@@ -18,8 +18,6 @@ class ViewHistsService {
       filters.status_cd = status_cd;
     }
 
-    console.log("Filtros finais:", filters);
-
     const foundHists = await prisma.biblioStatusHist.findMany({
       where: filters,
       orderBy: {
@@ -27,7 +25,12 @@ class ViewHistsService {
       },
       take: limit && limit > 0 ? limit : undefined,
       include: {
-        member: true, 
+        member: true,
+        biblio_copy: {
+          select: {
+            barcode_nmbr: true
+          }
+        }
       },
     });
 
@@ -35,24 +38,8 @@ class ViewHistsService {
       throw new Error("Nenhum histórico encontrado!");
     }
 
-    const copyIds = [...new Set(foundHists.map(h => h.copyid))];
-
-    const copyStatuses = await prisma.biblioCopy.findMany({
-      where: { id: { in: copyIds } },
-      select: { id: true, status_cd: true }
-    });
-
-    const statusMap = new Map(copyStatuses.map(copy => [copy.id, copy.status_cd]));
-
-    const updatedHists = foundHists.map(hist => ({
-      ...hist,
-      status_cd: statusMap.get(hist.copyid) || hist.status_cd,
-      firstname: hist.member?.first_name || null, 
-    }));
-
-    return updatedHists;
+    return foundHists;
   }
 }
-
 
 export default ViewHistsService;
