09dd9529e66eb5f54170eb7d0dcb253f5be4723c
Caahmos <dev.alexmorales@gmail.com>
2025-07-21 13:02:00 -0300

    feat: ViewCheckoutPrivs show more infos

--- src/services/CheckoutPrivs/ViewCheckPrivsService.ts
+++ src/services/CheckoutPrivs/ViewCheckPrivsService.ts
@@ -2,7 +2,20 @@ import prisma from "../../prisma/prisma";
 
 class ViewCheckPrivsService{
     static async execute(){
-        const checkprivs = await prisma.checkoutPrivs.findMany();
+        const checkprivs = await prisma.checkoutPrivs.findMany({
+            include: {
+                material_type_dm: {
+                    select: {
+                        description: true
+                    }
+                },
+                mbr_classify_dm: {
+                    select: {
+                        description: true
+                    }
+                }
+            }
+        });
 
         if(checkprivs.length <= 0) throw new Error('Nenhum dado encontrado!');
 
